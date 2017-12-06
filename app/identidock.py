93c4e5e1d704cd4d82a30268c76152e33c388653
kojima.kazuya <kojima.kazuya@Donuts-N558.local>
2017-12-06 10:50:33 +0900

    test2

--- app/identidock.py
+++ app/identidock.py
@@ -7,7 +7,8 @@ import html
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
-default_name = 'Joe Bloggs'
+# default_name = 'Joe Bloggs'
+default_name = 'KK'
 
 
 @app.route('/', methods=['GET', 'POST'])
